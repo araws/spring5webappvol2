0775dcae94a447de5a248bd1f44a955602bdf8da
rawsk <rawski.andrzej@gmail.com>
2019-12-16 12:47:29 +0100

    Initial commit

---
+++ src/main/java/pl/antygravity/spring5webappvol2/Spring5webappvol2Application.java
@@ -0,0 +1,13 @@
+package pl.antygravity.spring5webappvol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappvol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappvol2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/antygravity/spring5webappvol2/Spring5webappvol2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.antygravity.spring5webappvol2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappvol2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
