1f0b1d674ff033bf97d96711d562c31dba8fc2b6
rawsk <rawski.andrzej@gmail.com>
2019-12-18 12:36:53 +0100

    add data to repositories

--- src/main/java/pl/antygravity/spring5webappvol2/bootstrap/DevBootstrap.java
+++ src/main/java/pl/antygravity/spring5webappvol2/bootstrap/DevBootstrap.java
@@ -1,11 +1,30 @@
 package pl.antygravity.spring5webappvol2.bootstrap;
 
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 import pl.antygravity.spring5webappvol2.model.Author;
 import pl.antygravity.spring5webappvol2.model.Book;
+import pl.antygravity.spring5webappvol2.repository.AuthorRepository;
+import pl.antygravity.spring5webappvol2.repository.BookRepository;
 
-public class DevBootstrap {
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-    private void initData (){
+    AuthorRepository authorRepository;
+    BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
 
         //Anita
         Author anita = new Author("Anita", "Bean");
@@ -13,10 +32,16 @@ public class DevBootstrap {
         anita.getBooks().add(zws);
         zws.getAuthors().add(anita);
 
+        authorRepository.save(anita);
+        bookRepository.save(zws);
+
         //Jeff
         Author jeff = new Author("Jeff", "Galloway");
         Book tmb = new Book("Trening mentalny biegacza", "978-83-246-4873-3", "Helion");
         jeff.getBooks().add(tmb);
         tmb.getAuthors().add(jeff);
+
+        authorRepository.save(jeff);
+        bookRepository.save(tmb);
     }
 }
